d470170ef91702eafbb1e2a2bd0e4f099a46d8b2
nimbomx <ernesto@nimbo.mx>
2015-03-29 22:27:49 -0600

    9

--- www/js/slidetounlock.js
+++ www/js/slidetounlock.js
@@ -42,5 +42,25 @@ $(function() {
 	    this.addEventListener( 'webkitTransitionEnd', function( event ) { this.style.webkitTransition = 'none'; }, false );
 	    this.style.webkitTransform = 'translateX(397px)';
 	}, false);
+	
+	$('.slider')[0].addEventListener('touchmove', function(event) {
+	    event.preventDefault();
+	    var el = event.target;
+	    var touch = event.touches[0];
+	    var w = $(window).width();
+	    curX = touch.pageX - this.offsetLeft - ((w/2)-200);
+	    if(curX <= 0) return;
+	    if(curX > 397) return;
+	    
+	    if(curX < 50){
+	    	startSurvey();
+	    }
+	   	el.style.webkitTransform = 'translateX(' + curX + 'px)'; 
+	}, false);
+	$('.slider')[0].addEventListener('touchend', function(event) {	
+	    this.style.webkitTransition = '-webkit-transform 0.3s ease-in';
+	    this.addEventListener( 'webkitTransitionEnd', function( event ) { this.style.webkitTransition = 'none'; }, false );
+	    this.style.webkitTransform = 'translateX(397px)';
+	}, false);
 
 });
